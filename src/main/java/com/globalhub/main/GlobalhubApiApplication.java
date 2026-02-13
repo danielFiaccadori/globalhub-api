7ef0323c1548cca93f4001b6b3f111b7117a6581
danielFiaccadori <daniel.borborema2@hotmail.com>
2026-02-13 19:06:16 -0300

    Initial commit: GlobalHub API setup

---
+++ src/main/java/com/globalhub/main/GlobalhubApiApplication.java
@@ -0,0 +1,13 @@
+package com.globalhub.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalhubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalhubApiApplication.class, args);
+	}
+
+}
