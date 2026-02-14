0b99ba2fdfc87b06d75c1c68130541558aa799f5
danielFiaccadori <daniel.borborema2@hotmail.com>
2026-02-14 12:37:08 -0300

    feat: creation of the primitive User and UserRole classes

---
+++ src/main/java/com/globalhub/main/domain/user/User.java
@@ -0,0 +1,30 @@
+package com.globalhub.main.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table(name = "tb_users")
+@Entity(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id @Column(unique = true)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+}
---
+++ src/main/java/com/globalhub/main/domain/user/UserRole.java
@@ -0,0 +1,19 @@
+package com.globalhub.main.domain.user;
+
+public enum UserRole {
+
+    ADMIN("Admin"),
+    TEACHER("Teacher"),
+    STUDENT("Student");
+
+    private final String description;
+
+    UserRole(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
